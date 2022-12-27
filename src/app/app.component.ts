69cad434997d3f901102983d8560f50016695a5e
Amit Silam <m1084912@mindtree.com>
2022-12-27 16:49:55 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiserviceService } from './apiservice.service';
+import { EmpData } from './emp-data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularcrudApp';
+  listEmployees!: EmpData[];
+  constructor(private _apiservice:ApiserviceService){  }
+
+  ngOnInit(){
+    this.fetchEmp();
+  }
+  dataSource: any;
+  fetchEmp(){
+    this._apiservice.getData().subscribe(data=>{
+     this.listEmployees =data.data;
+     console.log("abc", this.listEmployees)
+     this.dataSource =new MatTableDataSource(this.listEmployees);
+     console.log("list", this.dataSource)
+    })
+  }
+
+
+  displayedColumns: string[] = ['id', 'employee_name', 'employee_salary', 'employee_age'];
+  // dataSource = new MatTableDataSource ELEMENT_DATA;
 }
